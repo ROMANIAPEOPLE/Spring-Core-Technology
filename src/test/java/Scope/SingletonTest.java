e58873ae3063e271db933a029b7d1c65193f1f14
jungkh405@naver.com <jungkh405@naver.com>
2020-12-18 20:33:00 +0900

    싱글톤 스코프 생성

---
+++ src/test/java/Scope/SingletonTest.java
@@ -0,0 +1,30 @@
+package Scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonTest {
+
+
+    @Test
+    void singletonBeanFind() {
+
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destory() {
+
+        }
+    }
+
+}
